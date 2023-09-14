ff4e234345eaabcc5034e9b639d4d43918777c4e
Vaza Aprilian Habar <133773621+Vazaaprilian@users.noreply.github.com>
2023-09-14 21:37:56 +0800

    Update navbar.php

--- include/navbar.php
+++ include/navbar.php
@@ -1,6 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-navbar fixed-top">
   <div class="container">
-    <img src="img/Logo_BPPT.png" width="10%">
     <a class="navbar-brand mb-2" href="index.php">Aplikasi Arsip</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
